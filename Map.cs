1a027b91c82b125608c0faceca1e742eabdc4420
Adam Cummins <adamrobertcummins@gmail.com>
2021-03-08 17:59:13 -0500

    initial commit, added menu

---
+++ Map.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Map
+{
+	public Map()
+	{
+		List<List<string>> myList = new List<List<string>>();
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+//using System.Collection.Generic;
+
+namespace Battleship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your name:  ");
+                var player = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("          Welcome to Battleship, {0}!", player);
+            Console.WriteLine(string.Concat(Enumerable.Repeat("=", 50)));
+            Console.WriteLine(string.Concat(Enumerable.Repeat("=", 20)) + "BattleShip" + string.Concat(Enumerable.Repeat("=", 20)));
+            Console.WriteLine(string.Concat(Enumerable.Repeat("=", 50)));
+            Console.WriteLine();
+            Console.WriteLine("You have 40 torpedos to try and destroy the 5 enemy ships.");
+            Console.WriteLine("There is a Aircraft Carrier, Battleship, Submarine, Crusier, & Destroyer.");
+            Console.WriteLine();
+            Console.Write("Do you want to start a game? Enter 'Y' to start, or any other key to exit:  ");
+                var check = Console.ReadLine();
+                var GuessCount = 0;
+            while (check == "Y" )
+                {
+                    Console.WriteLine("pass");
+                   // while (true) I got to figure out how to make a map here
+                    //{
+                      //  BuildMap();
+                   // }
+                   Console.Write("Do you want to play again? Enter 'Y' to start, or any other key to exit:  ");
+                   check = Console.ReadLine();
+                }
+            
+        }
+
+    }
+}
